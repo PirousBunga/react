ae1736ad1308aebfa62a393c59f4144029da2845
Macbook <pirousbung@yahoo.fr>
2022-11-07 23:22:55 +0100

    REACT

---
+++ cours-react1/src/pages/About.js
@@ -0,0 +1,55 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+        doloremque nemo tempore ad? Suscipit soluta nihil eveniet, sit in
+        asperiores dolore. Exercitationem, saepe earum! Similique alias quaerat
+        sit officia deserunt suscipit debitis numquam voluptatibus! Nam, eum?
+        Aspernatur error, voluptatum repudiandae nihil, animi placeat vero
+        aliquam id eligendi facilis quisquam. Sit doloremque similique
+        aspernatur odio vel tenetur necessitatibus unde, voluptate voluptatibus
+        quisquam minus fugit id esse aut dolorem molestias beatae itaque ratione
+        at eos exercitationem voluptatum magni. Voluptatibus delectus ex odit
+        temporibus unde dolorum atque rem, quam suscipit omnis quisquam expedita
+        veniam laboriosam facere. Tenetur modi autem esse quam? Sint, quasi.
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi atque
+        minus cupiditate, sunt, vero eaque sed perferendis reiciendis voluptatem
+        consequatur debitis ullam, totam quasi quaerat ex laborum dolorem
+        eveniet quae sit eos. Illo quo quia, omnis amet eveniet dignissimos
+        maxime doloremque nobis tenetur eligendi qui velit aliquam cupiditate
+        necessitatibus debitis. Voluptates totam facere sequi quo, ex reiciendis
+        perferendis molestiae error quaerat adipisci laborum inventore suscipit
+        odit, repellendus tenetur tempore! Veniam, deserunt maxime minima
+        commodi maiores fugit exercitationem? Enim natus modi dignissimos a.
+        Odit consequatur ab animi voluptatem facere alias ipsam, praesentium a,
+        quas nobis quasi vero quisquam autem dolorem maxime quia. Illum,
+        assumenda voluptates? Quidem hic perferendis voluptates impedit dolore
+        doloremque atque ratione eius sed adipisci dolores perspiciatis
+        accusamus quia rem iure debitis deleniti ab aliquid illo quam,
+        dignissimos quisquam nemo accusantium facilis! Tempora adipisci commodi
+        necessitatibus magnam sit sed id enim libero assumenda provident
+        deserunt suscipit expedita illum culpa laborum temporibus quisquam,
+        maxime minima. Dolore quaerat saepe molestiae harum esse accusantium
+        corrupti eaque numquam laboriosam fugiat! Eaque quae maiores facilis
+        voluptate sapiente, repellendus alias rem impedit, atque porro aperiam
+        pariatur ab aspernatur voluptatibus exercitationem aut, inventore
+        praesentium aliquid! Possimus fugit culpa esse eos consequuntur,
+        corrupti dolor sequi placeat maxime!
+      </p>
+    </div>
+  );
+};
+
+export default About;
