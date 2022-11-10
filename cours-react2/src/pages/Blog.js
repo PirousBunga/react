147402265ff2ec402918b19f624d318f39815014
Macbook <pirousbung@yahoo.fr>
2022-11-10 22:08:07 +0100

    React 2 to continue

--- cours-react2/src/pages/Blog.js
+++ cours-react2/src/pages/Blog.js
@@ -1,11 +1,23 @@
+import axios from "axios";
 import React from "react";
+import { useEffect } from "react";
 import { useState } from "react";
+import Article from "../components/Article";
 import Logo from "../components/Logo";
 import Navigation from "../components/Navigation";
 
 const Blog = () => {
+  const [blogData, setBlogData] = useState([]);
   const [content, setContent] = useState("");
   const [error, setError] = useState(false);
+
+  const getData = () => {
+    axios
+      .get("http://localhost:3004/articles")
+      .then((res) => setBlogData(res.data));
+  };
+
+  useEffect(() => getData(), []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -33,7 +45,13 @@ const Blog = () => {
         {error && <p>Veuillez écrire un minimum de 140 caractères</p>}
         <input type="submit" value="Envoyer" />
       </form>
-      <ul></ul>
+      <ul>
+        {blogData
+          .sort((a, b) => b.date - a.date)
+          .map((article) => (
+            <Article key={article.id} article={article} />
+          ))}
+      </ul>
     </div>
   );
 };
