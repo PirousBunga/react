52d2b338e8b8cf52bcd5331c93d00ee2e8556f20
Macbook <pirousbung@yahoo.fr>
2022-11-11 00:05:37 +0100

    Fin Cours React 1/2

--- cours-react2/src/pages/Blog.js
+++ cours-react2/src/pages/Blog.js
@@ -8,6 +8,7 @@ import Navigation from "../components/Navigation";
 
 const Blog = () => {
   const [blogData, setBlogData] = useState([]);
+  const [author, setAuthor] = useState("");
   const [content, setContent] = useState("");
   const [error, setError] = useState(false);
 
@@ -25,7 +26,15 @@ const Blog = () => {
     if (content.length < 140) {
       setError(true);
     } else {
+      axios.post("http://localhost:3004/articles", {
+        author,
+        content,
+        date: Date.now(),
+      });
       setError(false);
+      setAuthor("");
+      setContent("");
+      getData();
     }
   };
 
@@ -36,11 +45,17 @@ const Blog = () => {
       <h1>Blog</h1>
 
       <form onSubmit={(e) => handleSubmit(e)}>
-        <input type="text" placeholder="Nom" />
+        <input
+          type="text"
+          placeholder="Nom"
+          onChange={(e) => setAuthor(e.target.value)}
+          value={author}
+        />
         <textarea
           style={{ border: error ? "1px solid red" : "1px solid #61dafb" }}
           placeholder="Message"
           onChange={(e) => setContent(e.target.value)}
+          value={content}
         ></textarea>
         {error && <p>Veuillez écrire un minimum de 140 caractères</p>}
         <input type="submit" value="Envoyer" />
