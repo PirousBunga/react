4bc8637b5f6269cc98fc69be07b1bcd1cf70b62b
Macbook <pirousbung@yahoo.fr>
2022-11-19 16:13:20 +0100

    Fin de React Site Vitrine

--- react-vitrine/src/App.js
+++ react-vitrine/src/App.js
@@ -7,19 +7,22 @@ import Project1 from "./pages/Project1";
 import Project2 from "./pages/Project2";
 import Project3 from "./pages/Project3";
 import Project4 from "./pages/Project4";
+import { AnimatePresence } from "framer-motion";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Projet-1" element={<Project1 />} />
-        <Route path="/Projet-2" element={<Project2 />} />
-        <Route path="/Projet-3" element={<Project3 />} />
-        <Route path="/Projet-4" element={<Project4 />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <AnimatePresence>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Projet-1" element={<Project1 />} />
+          <Route path="/Projet-2" element={<Project2 />} />
+          <Route path="/Projet-3" element={<Project3 />} />
+          <Route path="/Projet-4" element={<Project4 />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </AnimatePresence>
     </BrowserRouter>
   );
 };
--- react-vitrine/src/pages/Contact.js
+++ react-vitrine/src/pages/Contact.js
@@ -1,10 +1,83 @@
 import React from "react";
+import CopyToClipboard from "react-copy-to-clipboard";
+import Buttons from "../components/Buttons";
+import ContactForm from "../components/ContactForm";
+import Logo from "../components/Logo";
+import Mouse from "../components/Mouse";
+import Navigation from "../components/Navigation";
+import SocialNetwork from "../components/SocialNetwork";
+import { motion } from "framer-motion";
 
 const Contact = () => {
+  const pageTransition = {
+    in: {
+      opacity: 1,
+      x: 0,
+    },
+    out: {
+      opacity: 0,
+      x: 200,
+    },
+  };
+
   return (
-    <div>
-      <h1>CONTACT</h1>
-    </div>
+    <main>
+      <Mouse />
+      <motion.div
+        className="contact"
+        initial="out"
+        animate="in"
+        exit="out"
+        variants={pageTransition}
+        transition={{ duration: 0.4 }}
+      >
+        <Navigation />
+        <Logo />
+        <ContactForm />
+        <div className="contact-infos">
+          <div className="adress">
+            <div className="content">
+              <h4>adresse</h4>
+              <p>12 rue Laplace</p>
+              <p>64200 Biarritz</p>
+            </div>
+          </div>
+          <div className="phone">
+            <div className="content">
+              <h4>Téléphone</h4>
+              <CopyToClipboard text="0642844284" className="hover">
+                <p
+                  style={{ cursor: "pointer" }}
+                  className="clipboard"
+                  onClick={() => alert("Téléphone copié !")}
+                >
+                  06 42 84 42 84
+                </p>
+              </CopyToClipboard>
+            </div>
+          </div>
+          <div className="email">
+            <div className="content">
+              <h4>email</h4>
+              <CopyToClipboard text="pdevagency@gmail.com" className="hover">
+                <p
+                  style={{ cursor: "pointer" }}
+                  className="clipboard"
+                  onClick={() => alert("Email copié !")}
+                >
+                  pdevagency@gmail.com
+                </p>
+              </CopyToClipboard>
+            </div>
+          </div>
+          <SocialNetwork />
+          <div className="credits">
+            <p>PirousDev - 2022</p>
+          </div>
+        </div>
+        <Buttons left={"/projet-4"} />
+      </motion.div>
+    </main>
   );
 };
 
